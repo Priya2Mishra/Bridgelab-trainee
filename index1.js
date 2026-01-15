2ab4aad821bc65119a6003520d37463ae73e9e16
Priya Mishra <shrijimishra6@gmail.com>
2026-01-15 14:38:31 +0530

    String Reversed

---
+++ index1.js
@@ -0,0 +1,13 @@
+let score;
+let data = null;
+
+console.log(score);
+console.log(data);
+
+let user = {
+    name: "Rahul",
+    age:25,
+    email:"rahul@gmail.com"
+};
+console.log(user);
+console.log(user.name);
