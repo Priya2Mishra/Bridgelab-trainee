cec5b1f713a781f5a06a766e1ccf26f3d03641a6
Priya Mishra <shrijimishra6@gmail.com>
2026-01-15 15:18:19 +0530

    Initial commit

--- index1.js
+++ index1.js
@@ -11,3 +11,25 @@ let user = {
 };
 console.log(user);
 console.log(user.name);
+
+let numbers = [10,20,30,40];
+
+// operators
+let a =10;
+let b = 5;
+
+console.log("Add:",a+b);
+console.log("Sub:",a-b);
+console.log("Mul:",a*b);
+console.log("Div:",a/b);
+
+// condition (if-else)
+let loginAge = 18;
+if(loginAge>=18){
+    console.log("Allowed to login");
+}
+else{
+    console.log("Not allowed to login");
+}
+
+
